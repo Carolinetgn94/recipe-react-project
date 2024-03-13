6eb1423fb9b30cbdc41003c17a8aa36efcf3fc29
Caroline <gekniateh@gmail.com>
2024-03-13 22:22:36 +0800

    Added little header css. create useContext() and fetch API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import {Routes, Route} from "react-router-dom"
+import {Route} from "react-router-dom"
 import './App.css';
 import Home from "./Home";
 import Favourites from "./Favourites";
 import Details from "./Details";
 import Header from "./Header";
+import { useState, createContext } from "react";
+
+export const dataContext = createContext('');
+console.log("dataContext", dataContext)
 
 function App() {
+const [searchInput, setSearchInput] = useState("");
+const [recipeList, setRecipeList] = useState([])
+
+async function handleSubmit(e){
+  e.preventDefault();
+  try {
+    const res = await fetch
+    (`https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchInput}&key=1ac39aa1-35ce-45f2-a54e-ea0270e69df6
+    `);
+    
+    const data = await res.json();
+    if(data.data.recipes) {
+      setRecipeList(data.data.recipes)
+      setSearchInput("")
+    }
+    console.log(data)
+  } catch(e) {
+    console.log(e);
+  }
+}
+console.log(recipeList)
+
   return (
     <div className="App">
-      <Header />
+      <dataContext.Provider value={{searchInput, setSearchInput, handleSubmit}}>
+        <Header />
+      </dataContext.Provider>
+      
          <main>
         <Route exact path="/">
           <Home />
--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,37 @@
+import { Link } from "react-router-dom"
+import './Header.css'
+import { useContext } from "react"
+import { dataContext } from "./App"
+
 function Header() {
+    const {searchInput, setSearchInput, handleSubmit} = useContext(dataContext);
+    console.log(searchInput)
+
     return (
-        <div>
-            <h1>Recipes4U</h1>
+        <div className="header">
+            <div className="recipe-logo">
+               <h1>
+                <Link to={'/'}>Recipes4U</Link>
+                </h1> 
+            </div>
+            <form onSubmit={handleSubmit}>
+                <input 
+                type="text" 
+                name="search" 
+                placeholder="What recipes do u want?" 
+                className="searchbar"
+                value={searchInput}
+                onChange={(e) => setSearchInput(e.target.value)}>
+                </input>
+            </form>
+            <div className="menu-items">
+                <h2>
+                    <Link to={'/'}>Home</Link>                
+                </h2>
+                <h2>
+                     <Link to={'/favourites'}>Favourites</Link>
+                </h2>
+            </div>
         </div>
     )
 }
